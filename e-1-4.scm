b82db621eed4d19f4ef5df697a03653266f63edf
Łukasz Korecki <lukasz@coffeesounds.com>
2014-02-16 16:04:09 +0000

    tweak

--- e-1-4.scm
+++ e-1-4.scm
@@ -1,7 +1,7 @@
 ; exercise 1.4
-(define (sorted a b c)
-  (sort  (list a b c) (lambda (a b) (< a b))))
+(define (sorted lst)
+  (sort  lst (lambda (a b) (< a b))))
 
 (define (square x) (* x x))
 
-(print (foldl + 0 (map  square (cdr (sorted 2 3 1)))))
+(print (foldl + 0 (map  square (cdr (sorted (list 2 3 1))))))
